95a37698877f40de0efaea2d81828512a1eb73c5
swaloure <orazayyeva@gmail.com>
2026-03-30 12:43:41 +0500

    Updates

--- _next/static/dVU29v-3icCBzm0Lm6mCX/_buildManifest.js
+++ _next/static/dVU29v-3icCBzm0Lm6mCX/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/heat6//_next/:path+",
+        "destination": "/heat6/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
